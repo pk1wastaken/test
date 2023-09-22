a3ca55b5e2e0e4c768d83707f4e771064c287511
pk1wastaken <117990545+pk1wastaken@users.noreply.github.com>
2023-09-23 00:39:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,83 @@
 import streamlit as st
-import numpy as np
-import string
 import xgboost
 import pickle
 import os
+import nltk
 from pathlib import Path
-current_directory = Path(__file__).parent #Get current directory
-file = open(os.path.join(current_directory, 'food_price.pkl'), 'rb')#rb = read bytes because we are reading the file
-model = pickle.load(file) #We give it the file and it loads in everything from the file. That data is the loaded as the variable model
-#filename = '../food_price.pkl'
-#model = pickle.load(open(os.path.join(filename, 'rb')))
-# st.set_option('deprecation.showfileUploaderEncoding',False)
-#model = pickle.load(open('food_price.pkl','rb'))
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
+import transformers
+#current_directory = Path(__file__).parent #Get current directory
+#file = open(os.path.join(current_directory, 'bestBERT.pkl'), 'rb')
 
+
+# Load the trained model from the .pkl file
+with open('bestBERT.pkl', 'rb') as file:
+    model = pickle.load(file)
+def anonymisation (text , first_party , second_party):
+    
+    first_words = first_party.split()
+    second_words = second_party.split()
+    
+    nltk_results = ne_chunk(pos_tag(word_tokenize(text)))
+    
+    for nltk_result in nltk_results:
+        if type(nltk_result) == Tree:
+            name = ''
+            for nltk_result_leaf in nltk_result.leaves():
+                name += nltk_result_leaf[0] + ' '
+        
+                #print ('Type: ', nltk_result.label(), 'Name: ', name)
+                if nltk_result.label() != 'PERSON':
+                    text = text.replace(name,'anonymized')
+            
+    text = text.replace(first_party ,'first_party') 
+    text = text.replace(second_party , 'second_party')
+    for f_w in first_words:
+        text = text.replace(f_w , 'first_party')
+    for s_w in second_words :
+        text = text.replace(s_w , 'second_party')
+                            
+    return text
+    
+def convert_text (text):
+
+  tokenn = tokenizer.encode_plus(text ,
+      max_length=256,
+      truncation = True,
+      padding='max_length',
+      add_special_tokens=True,
+      return_tensors='tf')
+  return tokenn.input_ids
+
+w=0
+v=0
 
 def main():
-    st.markdown("<h1 style='text-align: center; color: White;background-color:#e84343'>Food Price Predictor</h1>",
-                unsafe_allow_html=True)
-    st.markdown("<h3 style='text-align: center; color: Black;'>Drop in The required Inputs and we will do  the rest.</h3>", unsafe_allow_html=True)
-    st.markdown("<h4 style='text-align: center; color: Black;'>Submission for XGBoost Hamoye Internship Project</h4>", unsafe_allow_html=True)
-    st.sidebar.header("What is this Project about?")
-    st.sidebar.text(
-        "It  is a Web app that would help the user in determining whether he/she will get the right food price in a territory.")
-    st.sidebar.header("What tools where used to make this?")
-    st.sidebar.text("The Model was made using a dataset supplied by hamoye along with using Google Colab notebooks to train the model. We made use of XGBboost in order to make our Xtreme Gradient Boosting Model.")
+    st.markdown("<h1 style='text-align: center; color: White;background-color:#e84343'>Legal Case Predictor</h1>",unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; color: Black;'>Please provide me with the first party name, second party name, and all the available facts using said party names.</h3>", unsafe_allow_html=True)
+    
+    p1 = st.text_input("Enter first party name:")
+    p2 = st.text_input("Enter second party name:")
 
-    year_recorded = st.slider("Input Your Year", 0.0, 10.0)
-    country_id = st.slider("Input your country Score", 0, 340)
-    locality_id = st.slider("Input your locality_id Score", 0, 120)
-    quantity = st.slider("What Quantity?", 0, 100)
-    uni_rating = st.slider(
-        "Rating of the market type you wish to buy in on a Scale 1-5", 1, 5)
-
-    #passenger_data = {'country_id':1,'country_name':2,'locality_id':5,'market_id':10,'market_name':21,'commodity_purchase_id':8,'commodity_purchased':4,'currency_id':67,'name_of_currency':0.8,'market_type_id':2,'market_type':8,'measurement_id':12,'unit_of_goods_measurement':55,'month_recorded':4,'year_recorded':7,'price_paid':0.12,'quantity':83,'price_paid_usd':19,'measurement_unit':1}
-    # a is defined user input
-    #a = np.arange(1,18)
-    #a = a.reshape(1,-1)
-    a = np.random.randint(0, 100, size=(1, 17))
-    a = a.reshape(1, -1)
-
-    # country_id	country_name	locality_id	market_id	market_name	commodity_purchase_id	commodity_purchased	currency_id	name_of_currency	market_type_id	market_type	measurement_id	unit_of_goods_measurement	month_recorded	year_recorded	price_paid	quantity	price_paid_usd	measurement_unit
-    #inputs = [[year_recorded,country_id,locality_id,quantity,uni_rating]]
-    #inputs = np.array(inputs)
-
+    st.write("Please enter all the facts pertaining to the case file.(use the given party names)")
+    text= st.text_input("facts:")
+    prompt = anonymisation(text,p1,p2)
+    xx= convert_text(prompt)
+    
     if st.button('Predict'):
-        
-
-       #prediction = model.predict(df)
-
-       #prediction_proba = model.predict_proba(df)
-
-       # if st.butt
-
-       # And write the output:
-
-       # = np.array(['No','Yes'])
-
-       # st.write(churn_labels[prediction])
-
-       #st.subheader('Prediction Probability')
-
-       # st.write(prediction_proba)
+        y=model.predict(xx)
+        w=y[0][0] *100
+        v=y[0][1] *100
+        if y[0][0]> y[0][1] and y[0][0]>0.6 :
+            st.write(f"{p1} is predicted to win the case with a winning probability of {w}")
+        elif y[0][1]> y[0][0] and y[0][1]>0.6: 
+            st.write(f"{p2} is predicted to win the case with a winning probability of {v}")
+        else:
+            st.write(f"Couldnt predict the winner as the probability to win of {p1} is {w} and {p2} is {v}. This is way too close and could go in either way.")
 
 
 if __name__ == '__main__':
